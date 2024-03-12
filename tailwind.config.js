8aa3710189605e1ef17d2c79bfe3cd5531dd1ec1
zxamu <jsszmdpnc@outlook.com>
2024-03-11 22:11:06 -0600

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js"
+    ],
+    theme: {
+        extend: {
+            colors:{
+                'myblueg' : '#7abaca',
+                'myblueg2' : '#4257b1'
+            }
+        },
+    },
+    plugins: [
+        require('flowbite/plugin')
+    ],
+}
+
